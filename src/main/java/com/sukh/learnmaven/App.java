18ca438b96ea92440dceca57792fd121316a2370
faltu sharma <faltu.sharma1984@gmail.com>
2015-04-14 00:28:15 +0530

    first version of Code

---
+++ src/main/java/com/sukh/learnmaven/App.java
@@ -0,0 +1,16 @@
+package com.sukh.learnmaven;
+
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+		Logger logger = LoggerFactory.getLogger(App.class);
+		logger.info("Hello Added dependency successfully");
+    }
+}
